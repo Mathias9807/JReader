8221f3e2eed645b68145d35e7a5ea4d7c863cb61
Mathias Johansson <mathias9807@gmail.com>
2020-04-09 23:21:19 +0200

    Added swag extra word counter

--- page.js
+++ page.js
@@ -13,6 +13,7 @@ function importJSON(e) {
   writeUDict();
   writeODict();
   $("#known-words").html([...uDict].length + " words");
+  writeJapNumber();
 }
 $("#import").click(importJSON);
 
@@ -45,6 +46,7 @@ async function init() {
   oDict = await browser.runtime.sendMessage({request: 'getODict'});
 
   $("#known-words").html([...uDict].length + " words");
+  writeJapNumber();
 }
 init();
 
@@ -52,4 +54,24 @@ async function writeUDict() {
   return browser.runtime.sendMessage({request: 'writeUDict', dict: [...uDict]}); }
 async function writeODict() {
   return browser.runtime.sendMessage({request: 'writeODict', dict: [...oDict]}); }
+
+function getDigit(digit, ones=true) {
+  digit = Math.floor(digit) % 10;
+  if (!digit) return '';
+  if (!ones && digit == 1) return '';
+  return '一二三四五六七八九'.charAt(digit - 1);
+}
+function writeJapNumber() {
+  var number = [...uDict].length;
+  var numberStr = '' + number;
+
+  var str = '語';
+  if (numberStr.length >= 1) str = getDigit(number) + str;
+  if (numberStr.length >= 2) str = getDigit(number/10, false) + '十' + str;
+  if (numberStr.length >= 3) str = getDigit(number/100, false) + '百' + str;
+  if (numberStr.length >= 4) str = getDigit(number/1000, false) + '千' + str;
+  if (numberStr.length >= 5) str = getDigit(number/10000) + '万' + str;
+
+  $("#jap").html(str);
+}
 
