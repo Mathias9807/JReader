4f55d2437177f4ce6465aa74f0487ba03d4c9ab3
Mathias Johansson <mathias9807@gmail.com>
2020-08-28 21:06:58 +0200

    Disable sync storage, only allow local storage for the moment
    Previously would only write things to sync storage.

--- store.js
+++ store.js
@@ -9,32 +9,32 @@
 var uDict, oDict;
 
 async function loadUserDicts() {
-  var syncStorage = await browser.storage.sync.get(["uDict", "oDict"]);
+  // var syncStorage = await browser.storage.sync.get(["uDict", "oDict"]);
   var localStorage = await browser.storage.local.get(["uDict", "oDict"]);
 
   // Fetch user dictionary
   uDict = new Set();
-  var uDictStorage = syncStorage["uDict"];
-  if ($.isArray(uDictStorage) && uDictStorage.length > 0)
-    uDict = new Set(uDictStorage);
+  // var uDictStorage = syncStorage["uDict"];
+  // if ($.isArray(uDictStorage) && uDictStorage.length > 0)
+  //   uDict = new Set(uDictStorage);
   for (var l in localStorage["uDict"]) {
     uDict.add(l);
   }
 
   // Fetch unknown words dictionary
   oDict = new Set();
-  var oDictStorage = syncStorage["oDict"];
-  if ($.isArray(oDictStorage) && oDictStorage.length > 0)
-    oDict = new Set(oDictStorage);
+  // var oDictStorage = syncStorage["oDict"];
+  // if ($.isArray(oDictStorage) && oDictStorage.length > 0)
+  //   oDict = new Set(oDictStorage);
   for (var l in localStorage["oDict"]) {
     oDict.add(l);
   }
 }
 
 async function writeUDict() {
-  await browser.storage.sync.set({"uDict": [...uDict]});
+  await browser.storage.local.set({"uDict": [...uDict]});
 }
 async function writeODict() {
-  await browser.storage.sync.set({"oDict": [...oDict]});
+  await browser.storage.local.set({"oDict": [...oDict]});
 }
 
